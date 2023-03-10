d429ce40a65c75088f47b86ab99cdbb8fc6e308f
farahdawoud <121869547+farahdawoud@users.noreply.github.com>
2023-03-10 11:24:16 +0400

    Using new API key

--- App.tsx
+++ App.tsx
@@ -4,12 +4,12 @@ import {SafeAreaView} from 'react-native';
 import HomeScreenView from './src/screens/HomeScreen/HomeScreen.view';
 
 function App(): JSX.Element {
-  const xApiKey = 'ZDg4MTkyNGItMDg0ZS00M2NjLWIwYjQtY2VhNTc5YzUwYzhm'; // Your project key
+  const xApiKey = 'NmYxODE2OTItMjRlYi00ZDQ5LWIxNjEtNzlmYjQ4YTUxZmI5'; // Your project key
   const appInfo = {
     appId: 'id428927940', // Your app url in play store or app store
     appName: 'Demo', // Your app name
     appVersion: '0.1.0', // Your app version, you can get it using react-native-device-info
-    platform: 'android', // App Platform, android or ios
+    platform: 'ios', // App Platform, android or ios
     environment: 'dev', // App Environment, production, development
     appLanguage: 'en', //Your app language ex: en, es etc. Optional.
   };
